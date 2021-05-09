8fbf7e9a0e1401a75ab3bb39be7842f8900565bb
monkeyDugi <lbd4946@gmail.com>
2021-05-09 14:55:06 +0900

    sytle : getter, setter lombok 적용

--- src/main/java/com/devlogmoa/config/BlogPropertiesDto.java
+++ src/main/java/com/devlogmoa/config/BlogPropertiesDto.java
@@ -1,32 +1,12 @@
 package com.devlogmoa.config;
 
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter @Setter
 public class BlogPropertiesDto {
 
     private String url;
     private String rssUrl;
     private String description;
-
-    public String getUrl() {
-        return url;
-    }
-
-    public void setUrl(String url) {
-        this.url = url;
-    }
-
-    public String getRssUrl() {
-        return rssUrl;
-    }
-
-    public void setRssUrl(String rssUrl) {
-        this.rssUrl = rssUrl;
-    }
-
-    public String getDescription() {
-        return description;
-    }
-
-    public void setDescription(String description) {
-        this.description = description;
-    }
 }
