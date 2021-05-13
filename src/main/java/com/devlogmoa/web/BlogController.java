3b56f101e9f0673b2beb6126041d4a556a1eb5fe
monkeyDugi <lbd4946@gmail.com>
2021-05-13 21:53:58 +0900

    style : import 정리

--- src/main/java/com/devlogmoa/web/BlogController.java
+++ src/main/java/com/devlogmoa/web/BlogController.java
@@ -1,21 +1,15 @@
 package com.devlogmoa.web;
 
-import com.devlogmoa.domain.BlogDetail;
 import com.devlogmoa.repository.BlogDetailRepository;
 import com.devlogmoa.web.dto.response.BlogDetailDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
-import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
 import org.springframework.data.web.PageableDefault;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
-
-import java.util.List;
-import java.util.stream.Collectors;
 
 @RequiredArgsConstructor
 @Controller
