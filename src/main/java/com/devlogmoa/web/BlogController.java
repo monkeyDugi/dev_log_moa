80702eb658a8e0645268b7fcc9fef75b4802551a
monkeyDugi <lbd4946@gmail.com>
2021-05-15 12:23:05 +0900

    feat : 페이지 사이즈 10으로 설정

--- src/main/java/com/devlogmoa/web/BlogController.java
+++ src/main/java/com/devlogmoa/web/BlogController.java
@@ -1,13 +1,16 @@
 package com.devlogmoa.web;
 
+import com.devlogmoa.domain.BlogDetail;
 import com.devlogmoa.repository.BlogDetailRepository;
 import com.devlogmoa.web.dto.response.BlogDetailDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @RequiredArgsConstructor
 @Controller
@@ -16,9 +19,9 @@ public class BlogController {
     private final BlogDetailRepository blogDetailRepository;
 
     @GetMapping("/")
-    public String blogs(Model model, Pageable pageable) {
+    public String blogs(Model model, @PageableDefault(size = 10) Pageable pageable) {
         Page<BlogDetailDto> blogDetails = blogDetailRepository.findAllByOrderByPubDateDesc(pageable)
-                                                            .map(BlogDetailDto::new);
+                .map(BlogDetailDto::new);
 
         model.addAttribute("blogDetails", blogDetails);
 
