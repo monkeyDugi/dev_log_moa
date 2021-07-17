9940da153e8ce72360987451afee2c18efb23dc0
monkeyDugi <lbd4946@gmail.com>
2021-07-17 13:01:53 +0900

    style : import 와일드 카드 제거

--- src/main/java/com/devlogmoa/web/subscription/SubscriptionController.java
+++ src/main/java/com/devlogmoa/web/subscription/SubscriptionController.java
@@ -11,7 +11,11 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.data.web.PageableDefault;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @RequiredArgsConstructor
 @Controller
