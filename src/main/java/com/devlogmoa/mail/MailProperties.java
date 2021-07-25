e382eecf1410a6cc167bc8b5a75e25c7a90eecba
monkeyDugi <lbd4946@gmail.com>
2021-07-25 22:41:42 +0900

    issue #76 refactor - 발신자 메일링 정보 설정파일로 변경

---
+++ src/main/java/com/devlogmoa/mail/MailProperties.java
@@ -0,0 +1,19 @@
+package com.devlogmoa.mail;
+
+import com.devlogmoa.util.YamlPropertiesSourceFactory;
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.stereotype.Component;
+
+@Getter @Setter
+@Configuration
+@PropertySource(value = "classpath:application-mailing.yml", factory = YamlPropertiesSourceFactory.class)
+@ConfigurationProperties(prefix = "sender-mail")
+public class MailProperties {
+
+    private String id;
+    private String password;
+}
